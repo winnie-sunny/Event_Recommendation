04d7cec9a073e5ebe0a43e71cde44b1bf8d85689
Wen Jia <winniejia01@gmail.com>
2023-02-26 19:29:38 -0500

    handle API results

--- workspace/Jupiter/src/main/java/external/TicketMasterAPI.java
+++ workspace/Jupiter/src/main/java/external/TicketMasterAPI.java
@@ -4,9 +4,17 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import entity.Item;
+import entity.Item.ItemBuilder;
 
 public class TicketMasterAPI {
 	private static final String URL = "https://app.ticketmaster.com/discovery/v2/events.json";
@@ -59,6 +67,131 @@ public class TicketMasterAPI {
 		}
 		return new JSONArray();
               }
+  
+    private String getAddress(JSONObject event) throws JSONException {
+		if (!event.isNull("_embedded")) {
+			JSONObject embedded = event.getJSONObject("_embedded");
+			
+			if (!embedded.isNull("venues")) {
+				JSONArray venues = embedded.getJSONArray("venues");
+				
+				for (int i = 0; i < venues.length(); ++i) {
+					JSONObject venue = venues.getJSONObject(i);
+					
+					StringBuilder sb = new StringBuilder();
+					
+					if (!venue.isNull("address")) {
+						JSONObject address = venue.getJSONObject("address");
+						
+						if (!address.isNull("line1")) {
+							sb.append(address.getString("line1"));
+						}
+						if (!address.isNull("line2")) {
+							sb.append(" ");
+							sb.append(address.getString("line2"));
+						}
+						if (!address.isNull("line3")) {
+							sb.append(" ");
+							sb.append(address.getString("line3"));
+						}
+					}
+					
+					if (!venue.isNull("city")) {
+						JSONObject city = venue.getJSONObject("city");
+						
+						if (!city.isNull("name")) {
+							sb.append(" ");
+							sb.append(city.getString("name"));
+						}
+					}
+					
+					if (!sb.toString().equals("")) {
+						return sb.toString();
+					}
+				}
+			}
+		}
+
+		return "";
+	}
+
+	// {"images": [{"url": "www.example.com/my_image.jpg"}, ...]}
+	private String getImageUrl(JSONObject event) throws JSONException {
+		if (!event.isNull("images")) {
+			JSONArray images = event.getJSONArray("images");
+			
+			for (int i = 0; i < images.length(); ++i) {
+				JSONObject image = images.getJSONObject(i);
+				
+				if (!image.isNull("url")) {
+					return image.getString("url");
+				}
+			}
+		}
+
+		return "";
+	}
+
+	
+	// {"classifications" : [{"segment": {"name": "music"}}, ...]}
+	private Set<String> getCategories(JSONObject event) throws JSONException {
+		Set<String> categories = new HashSet<>();
+		if (!event.isNull("classifications")) {
+			JSONArray classifications = event.getJSONArray("classifications");
+			for (int i = 0; i < classifications.length(); i++) {
+				JSONObject classification = classifications.getJSONObject(i);
+				if (!classification.isNull("segment")) {
+					JSONObject segment = classification.getJSONObject("segment");
+					if (!segment.isNull("name")) {
+						String name = segment.getString("name");
+						categories.add(name);
+					}
+				}
+			}
+		}
+
+		return categories;
+	}
+
+	
+	// Convert JSONArray to a list of item objects.
+	private List<Item> getItemList(JSONArray events) throws JSONException {
+		List<Item> itemList = new ArrayList<>();
+
+		for (int i = 0; i < events.length(); ++i) {
+			JSONObject event = events.getJSONObject(i);
+			
+			ItemBuilder builder = new ItemBuilder();
+			
+			if (!event.isNull("name")) {
+				builder.setName(event.getString("name"));
+			}
+			
+			if (!event.isNull("id")) {
+				builder.setItemId(event.getString("id"));
+			}
+			if (!event.isNull("url")) {
+				builder.setUrl(event.getString("url"));
+			}
+			
+			if (!event.isNull("rating")) {
+				builder.setRating(event.getDouble("rating"));
+			}
+			
+			if (!event.isNull("distance")) {
+				builder.setDistance(event.getDouble("distance"));
+			}
+			
+			builder.setCategories(getCategories(event));
+			builder.setAddress(getAddress(event));
+			builder.setImageUrl(getImageUrl(event));
+			
+			itemList.add(builder.build());
+		}
+
+		return itemList;
+	}
+
 
     
     
